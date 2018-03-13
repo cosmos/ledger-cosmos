b4a9bd71aa615c698a3521015b17347132154a36
Przemek Stys <ppstys@gmail.com>
2018-03-13 23:01:31 +0000

    Update license header

--- src/ledger/src/app_main.h
+++ src/ledger/src/app_main.h
@@ -1,3 +1,19 @@
+/*******************************************************************************
+*   (c) 2016 Ledger
+*   (c) 2018 ZondaX GmbH
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
 #pragma once
 
 #include "JsonParser.h"
@@ -8,9 +24,10 @@
 #define OFFSET_PCK_INDEX 2  //< Package index offset
 #define OFFSET_PCK_COUNT 3  //< Package count offset
 #define OFFSET_DATA      4  //< Data offset
+#define JSON_BUFFER_SIZE 1000
 
 extern parsed_json_t parsed_json;
-extern char json_buffer[1000];
+extern char json_buffer[JSON_BUFFER_SIZE];
 
 extern uint32_t json_buffer_write_pos;
 extern uint32_t json_buffer_size;
--- src/lib/JsonParser.h
+++ src/lib/JsonParser.h
@@ -1,5 +1,20 @@
-// Distributed under the MIT software license, see the accompanying
-// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+/*******************************************************************************
+*   (c) 2016 Ledger
+*   (c) 2018 ZondaX GmbH
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+
 #ifndef CI_TEST_JSONPARSER_H
 #define CI_TEST_JSONPARSER_H
 
@@ -7,60 +22,16 @@
 #include <stdbool.h>
 #include <string.h>
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef  unsigned char byte;
+typedef unsigned char byte;
 
 #define MAX_NUMBER_OF_TOKENS    128
 #define MAX_JSON_DEPTH          6
 #define MAX_INPUT_OUTPUT_COUNT  2
 #define MAX_COIN_COUNT          3
-
-// List of JSON element in SendMsg (begin)
-// TODO: Remove it
-#define DF_NAME_INDEX                   1 //(string)
-#define V_NAME_INDEX                    3 //(string)
-#define V_VALUE_INDEX                   4 //(object)
-#define INPUTS_NAME_INDEX               5 //(string)
-#define INPUTS_VALUE_INDEX              6 //(array)
-
-// Repeated for every input in the inputs array
-#define INPUT_VALUE_INDEX               7  //(object)
-#define INPUT_ADDRESS_NAME_INDEX        8  //(string)
-#define INPUT_ADDRESS_VALUE_INDEX       9  //(string)
-#define INPUT_COINS_NAME_INDEX          10 //(string)
-#define INPUT_COINS_VALUE_INDEX         11 //(array)
-
-// Repeated for every coin in the coins array
-#define INPUT_COIN_INDEX                12 //(object)
-#define INPUT_COIN_DENOM_NAME_INDEX     13 //(object)
-#define INPUT_COIN_DENOM_VALUE_INDEX    14 //(object)
-#define INPUT_COIN_AMOUNT_NAME_INDEX    15 //(string)
-#define INPUT_COIN_AMOUNT_VALUE_INDEX   16 //(primitive)
-
-#define INPUT_SEQUENCE_NAME_INDEX       17 //(string)
-#define INPUT_SEQUENCE_VALUE_INDEX      18 //(primitive)
-
-#define OUTPUTS_NAME_INDEX               19 //(string)
-#define OUTPUTS_VALUE_INDEX              20 //(array)
-
-// Repeated for every input in the inputs array
-#define OUTPUT_VALUE_INDEX               21 //(object)
-#define OUTPUT_ADDRESS_NAME_INDEX        22 //(string)
-#define OUTPUT_ADDRESS_VALUE_INDEX       23 //(string)
-#define OUTPUT_COINS_NAME_INDEX          24 //(string)
-#define OUTPUT_COINS_VALUE_INDEX         25 //(array)
-
-// Repeated for every coin in the coins array
-#define OUTPUT_COIN_INDEX                26 //(object)
-#define OUTPUT_COIN_DENOM_NAME_INDEX     27 //(object)
-#define OUTPUT_COIN_DENOM_VALUE_INDEX    28 //(object)
-#define OUTPUT_COIN_AMOUNT_NAME_INDEX    29 //(string)
-#define OUTPUT_COIN_AMOUNT_VALUE_INDEX   30 //(primitive)
-// List of JSON element in SendMsg (end)
 
 // Part of the SendMsg struct
 typedef struct
