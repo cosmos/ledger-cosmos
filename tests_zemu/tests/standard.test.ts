d9c9dc618c708f6fb7adc80bb2395d85a52b99fb
Juan Leni <juan.leni@zondax.ch>
2021-09-16 23:47:07 +0200

    update zemu

--- tests_zemu/tests/standard.test.ts
+++ tests_zemu/tests/standard.test.ts
@@ -33,6 +33,10 @@ const defaultOptions = {
 
 jest.setTimeout(60000)
 
+beforeAll(async () => {
+  await Zemu.checkAndPullImage()
+})
+
 describe('Standard', function () {
   test.each(models)('can start and stop container', async function (m) {
     const sim = new Zemu(m.path)
