8b1ef2e6e838dd807450a5084f1a143ba647b514
Carlos Medeiros <carlos.medeiros@zondax.ch>
2025-02-21 19:00:54 +0000

    add chain support on get amount

--- app/src/swap/handle_get_printable_amount.c
+++ app/src/swap/handle_get_printable_amount.c
@@ -37,7 +37,11 @@ void handle_get_printable_amount(get_printable_amount_parameters_t *params) {
     memcpy(amount + COIN_AMOUNT_MAXSIZE - params->amount_length, params->amount, params->amount_length);
 
     char tmp_amount[110] = {0};
-    zxerr_t zxerr = bytesAmountToStringBalance(amount, sizeof(amount), tmp_amount, sizeof(tmp_amount));
+    int8_t chain_index = find_chain_index_by_coin_config((char *)&params->coin_configuration[1], params->coin_configuration[0]);
+    if (chain_index == -1) {
+        return;
+    }
+    zxerr_t zxerr = bytesAmountToStringBalance(amount, sizeof(amount), tmp_amount, sizeof(tmp_amount), chain_index);
 
     if (zxerr != zxerr_ok || strnlen(tmp_amount, sizeof(tmp_amount)) > sizeof(params->printable_amount)) {
         return;
