0f86d8cd48ca7fe3ded7fcc3bc758acd2c3d5cdc
Carlos Medeiros <carlos.medeiros@zondax.ch>
2026-04-01 10:15:28 +0100

    format file

--- app/src/swap/swap_utils.c
+++ app/src/swap/swap_utils.c
@@ -118,7 +118,7 @@ zxerr_t readU32BE(uint8_t *input, uint32_t *output) {
 
   *output = 0;
   for (uint8_t i = 0; i < 4; i++) {
-    *output += (uint32_t) * (input + i) << (32 - (8 * (i + 1)));
+    *output += (uint32_t)*(input + i) << (32 - (8 * (i + 1)));
   }
   return zxerr_ok;
 }
